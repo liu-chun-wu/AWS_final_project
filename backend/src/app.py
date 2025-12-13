6b5406a389c6dcc8308e0052eaae70b8dba13752
jefferyLiu <a4264428@gmail.com>
2025-12-13 20:31:55 +0800

    test CI

--- backend/src/app.py
+++ backend/src/app.py
@@ -73,7 +73,7 @@ def register_routes(app):
         """
         return jsonify({
             "status": "ok",
-            "service": "production-backend111502522"
+            "service": "production-backend111"
         }), 200
 
     @app.route('/generate-image', methods=['POST'])
